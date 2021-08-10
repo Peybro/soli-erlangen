b3ca9de80cf6c51471b6420b4f9fa4d8e6f343d0
Thomas Kretschmann <t.kretschmann@t-online.de>
2021-08-10 15:00:56 +0200

    pdf preview

--- src/pages/verein/zeitung.js
+++ src/pages/verein/zeitung.js
@@ -3,43 +3,80 @@ import Layout from "../../layouts/layout";
 import Accordion from "react-bootstrap/Accordion";
 
 export default function Zeitung() {
-    const title = "Vereinsblatt"
-    const setting = {
-        start: 2005,
-        last: {
-            year: 2020,
-            version: 2
-        },
-        link: "https://www.soli-erlangen.de/assets/vereinsblatt/soli_info_"
-    }
+  const title = "Vereinsblatt";
+  const setting = {
+    start: 2005,
+    last: {
+      year: 2020,
+      version: 2,
+    },
+    link: "https://www.soli-erlangen.de/assets/vereinsblatt/soli_info_",
+  };
 
-    function getYearArray() {
-        const array = []
-        for (let i = setting.start; i <= setting.last.year; i++) {
-            array.push(i)
-        }
-        return array.reverse()
+  function getYearArray() {
+    const array = [];
+    for (let i = setting.start; i <= setting.last.year; i++) {
+      array.push(i);
     }
+    return array.reverse();
+  }
 
-    return (
-        <Layout pageTitle={title}>
-            <h2 className="heading">{title}</h2>
-            <Accordion defaultActiveKey={setting.last.year}>
-                {getYearArray().map((year, i) => (
-                    <Accordion.Item eventKey={year} key={i}>
-                        <Accordion.Header>{year}</Accordion.Header>
-                        <Accordion.Body>
-                            <div className="row justify-content-around">
-                                <a type="button" className="col-4 btn btn-primary" href={`${setting.link}${year}_1.pdf`}
-                                   target="_blank" rel="noreferrer">April {year} - September {year}</a>
-                                {setting.last.version === 2 && <a type="button" className="col-4 btn btn-primary"
-                                                                  href={`${setting.link}${year}_2.pdf`}
-                                                                  target="_blank" rel="noreferrer">Oktober {year} -
-                                    März {year + 1}</a>}
-                            </div>
-                        </Accordion.Body>
-                    </Accordion.Item>))}
-            </Accordion>
-        </Layout>
-    );
+  return (
+    <Layout pageTitle={title}>
+      <h2 className="heading">{title}</h2>
+      <Accordion defaultActiveKey={setting.last.year}>
+        {getYearArray().map((year, i) => (
+          <Accordion.Item eventKey={year} key={i}>
+            <Accordion.Header>{year}</Accordion.Header>
+            <Accordion.Body>
+              <div className="row justify-content-around">
+                <div className="col">
+                  <h2>
+                    April {year} - September {year}
+                  </h2>
+                  <iframe
+                    src={`${setting.link}${year}_1.pdf`}
+                    loading="lazy"
+                    width="100%"
+                    height="300px"
+                  />
+                  <a
+                    type="button"
+                    className="btn btn-primary"
+                    href={`${setting.link}${year}_1.pdf`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Größer anzeigen
+                  </a>
+                </div>
+                {setting.last.version === 2 && (
+                  <div className="col">
+                    <h2>
+                      Oktober {year} - März {year + 1}
+                    </h2>
+                    <iframe
+                      src={`${setting.link}${year}_2.pdf`}
+                      loading="lazy"
+                      width="100%"
+                      height="300px"
+                    />
+                    <a
+                      type="button"
+                      className="btn btn-primary"
+                      href={`${setting.link}${year}_2.pdf`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Größer anzeigen
+                    </a>
+                  </div>
+                )}
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    </Layout>
+  );
 }
